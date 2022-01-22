fedb799b9376e62cc7e6476256b7168a922615b8
Daniel Dowse <support@mylinuxadmin.de>
2022-01-22 14:17:42 +0100

    Skript 08

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 apt-get update
 git clone https://github.com/yangxiaohua2009/custom-kernel.git
 cd custom-kernel
-unzip -q kernel.zip
+cat kernel.* > kernelnew.zip
+unzip -q kernelnew.zip
 tar -xvzf es8336-topologies-2.tar.gz
 chmod u+x make.sh
 ./make.sh
